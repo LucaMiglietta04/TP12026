c8d2ce1de2bf49c1c1dea1df60e1b6110bb91675
LucaMiglietta04 <lucamiglietta2004@gmail.com>
2026-04-20 10:31:20 -0300

    Añadido del boxplot GIRAI

---
+++ IA/boxplot_GIRAI.R
@@ -0,0 +1,22 @@
+# Instalo los paquetes necesarios (si aún no los tengo instalados)
+# install.packages("tidyverse")
+# install.packages("ggplot2")
+
+# Cargo los paquetes que voy a usar
+library(tidyverse)
+library(ggplot2)
+
+# Fijo el dataset
+attach(datos_limpios)
+
+
+
+datos_limpios %>% 
+  select(GIRAI) %>%
+
+  ggplot() +
+  aes(x = GIRAI, y = "") +
+  geom_boxplot(width = 0.50, fill = "lightblue", outlier.size = 1) +
+  theme(axis.ticks.y = element_blank()) +
+  labs(y = "", x = "Valor GIRAI") +
+  scale_x_continuous(breaks = seq(0, 250, 50)) # Marcas del eje
